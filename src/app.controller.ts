d8186316dbaa01a565ddf7ee647b9d98f663f478
Rogelio Gonzalez <Rogelio.Gonzalez@bitsamericas.com>
2023-10-04 08:50:43 -0400

    Apply single responsability

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,19 +8,5 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
-  }
-  @Get('products')
-  getProducts(
-    @Query('limit') limit = 100,
-    @Query('offset') offset = 0,
-    @Query('brand') brand: string,
-  ) {
-    return `products limit=> ${limit} with offset ${offset} y marca ${brand}`;
-  }
-
-  @Get('/product/:productID')
-  findOne(@Param('productID') productID: number) {
-    console.log(productID);
-    return `This action returns a #${productID} product`;
   }
 }
--- src/controllers/products/products.controller.ts
+++ src/controllers/products/products.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+  @Get()
+  getProducts(
+    @Query('limit') limit = 100,
+    @Query('offset') offset = 0,
+    @Query('brand') brand: string,
+  ) {
+    return `products limit=> ${limit} with offset ${offset} y marca ${brand}`;
+  }
+  @Get('/:productID')
+  findOne(@Param('productID') productID: number) {
+    console.log(productID);
+    return `This action returns a #${productID} product`;
+  }
+}
