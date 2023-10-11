a1af126949855893af72c044265ad5bb8263fc4d
Rogelio Gonzalez <Rogelio.Gonzalez@bitsamericas.com>
2023-10-11 19:17:45 -0400

    Use Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProductService } from './services/product/product.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [UsersModule, ProductsModule],
   controllers: [AppController],
-  providers: [AppService, ProductService],
+  providers: [AppService],
 })
 export class AppModule {}
